bde33fd7827ccf47608b106ec08d096ef7e3740c
Liam O'Neill <l.oneill20@nuigalway.ie>
2018-10-24 08:56:14 +0100

    Trying to do a little bit of TDD

---
+++ src/test/java/ie/nuigalway/it/oneill/liam/ct417/assignment/a/test/StudentRegistrationSystemTest.java
@@ -0,0 +1,49 @@
+package ie.nuigalway.it.oneill.liam.ct417.assignment.a.test;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Liam
+ */
+public class StudentRegistrationSystemTest {
+    
+    public StudentRegistrationSystemTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testGetUsername() {
+        Student student = new Student();
+        student.setName("Cathal Burke");
+        student.setDob(new Date(98, 10, 15)); // Will calculate age automatically
+        // Deciding that getUsername() wil lalso remove any spaces in the name.
+        assertEquals("CathalBurke20", student.getUsername());
+    }
+}
